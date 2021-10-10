bd9cdd5f9d78770db6930581f660f70110b6eb2b
Savage <mangevarun2006@gmail.com>
2021-10-10 18:55:13 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/main.scss";
+import React, { useEffect } from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const start = () => NProgress.start();
+    const end = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/location/[city].js
@@ -0,0 +1,99 @@
+import React from "react";
+import cities from "../../lib/city.list.json";
+import Head from "next/head";
+import moment from "moment";
+import TodaysWeather from "../../components/TodaysWeather";
+import HourlyWeather from "../../components/HourlyWeather";
+import WeeklyWeather from "../../components/WeeklyWeather";
+import SearchBox from "../../components/SearchBox";
+import Link from "next/link";
+
+export async function getServerSideProps(context) {
+  const city = getCity(context.params.city);
+
+  if (!city) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&exclude=minutely&units=imperial`
+  );
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      city: city,
+      timezone: data.timezone,
+      currentWeather: data.current,
+      dailyWeather: data.daily,
+      hourlyWeather: getHourlyWeather(data.hourly, data.timezone),
+    },
+  };
+}
+
+const getCity = (param) => {
+  const cityParam = param.trim();
+  const splitCity = cityParam.split("-");
+  const id = splitCity[splitCity.length - 1];
+
+  if (!id) {
+    return null;
+  }
+
+  const city = cities.find((city) => city.id.toString() == id);
+
+  if (city) {
+    return city;
+  } else {
+    return null;
+  }
+};
+
+const getHourlyWeather = (hourlyData, timezone) => {
+  const endOfDay = moment().tz(timezone).endOf("day").valueOf();
+  const eodTimeStamp = Math.floor(endOfDay / 1000);
+
+  const todaysData = hourlyData.filter((data) => data.dt < eodTimeStamp);
+
+  return todaysData;
+};
+
+export default function City({
+  city,
+  timezone,
+  currentWeather,
+  dailyWeather,
+  hourlyWeather,
+}) {
+  return (
+    <div>
+      <Head>
+        <title>{city.name} Weather - Next Weather App</title>
+      </Head>
+      <div className="page-wrapper">
+        <div className="container">
+          <Link href="/">
+            <a className="back-link">&larr; Home</a>
+          </Link>
+          <SearchBox placeholder={city.name} />
+          <TodaysWeather
+            city={city}
+            weather={dailyWeather[0]}
+            timezone={timezone}
+          />
+          <HourlyWeather hourlyWeather={hourlyWeather} timezone={timezone} />
+          <WeeklyWeather weeklyWeather={dailyWeather} timezone={timezone} />
+        </div>
+      </div>
+    </div>
+  );
+}
